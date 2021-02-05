b6e14c6bac168958d4daead1bf84335b40d469ea
arsh1512 <70216277+arsh1512@users.noreply.github.com>
2021-02-05 23:11:16 +0000

    Create FruitsKNN.py

---
+++ FruitsKNN.py
@@ -0,0 +1,12 @@
+#Importing Libraries and fruits data set
+
+%matplotlib notebook
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+#importing CSV file
+fruits = pd.read_csv(FILEPATH)
+
+
