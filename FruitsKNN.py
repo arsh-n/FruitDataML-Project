cb40e78f607cc50a920f1318bdaad0cf96d59824
arsh1512 <70216277+arsh1512@users.noreply.github.com>
2021-02-05 23:52:26 +0000

    Update FruitsKNN.py

--- FruitsKNN.py
+++ FruitsKNN.py
@@ -27,3 +27,22 @@ knn.score (X_test, y_test)
 #using the predictor for a new value
 fruit_prediction = knn.predict([[20, 4.3, 5.5]])
 lookup_fruit_name[fruit_prediction[0]]
+
+
+#testing and plotting range of k values to check which would be best fit
+k_range = range(1,20)
+scores = []
+
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors = k)
+    knn.fit(X_train, y_train)
+    scores.append(knn.score(X_test, y_test))
+
+plt.figure()
+plt.xlabel('k')
+plt.ylabel('accuracy')
+plt.scatter(k_range, scores)
+plt.xticks([0,5,10,15,20]);
+
+
+##end 
